7e1441d92bb2aa2742cdf7e7f9f9f3edbd919a8b
unknown <nickewideving@gmail.com>
2021-12-06 23:24:33 +0100

    removed env files?

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,7 +12,7 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const search = functions.https.onRequest(async (request, response) => {
-    requestify.get(`https://darkbyte.ent.europe-north1.gcp.elastic-cloud.com:443/api/as/v1/engines/darkbyte/search?auth_token=${process.env.SEARCH_TOKEN}&query=${request.body.query}`)
+    requestify.get(`${process.env.SEARCH_URL}${process.env.SEARCH_TOKEN}&query=${request.body.query}`)
         .then(function (res: any) {
             // Get the response body (JSON parsed or jQuery object for XMLs)
             response.send(res.getBody())
